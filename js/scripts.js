dd49116d49962e474b60146f7173d5809152f4a3
Seth Perts <seth.robert.perts@gmail.com>
2024-12-28 20:32:36 -0500

    Tweak

--- js/scripts.js
+++ js/scripts.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
         $(window).scroll(function () {
             var scroll = $(window).scrollTop();
 
-            if (scroll >= 20) {
+            // if (scroll >= 20) {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
@@ -101,19 +101,19 @@ $(document).ready(function () {
                 $('header .navicon').css({
                     "top": "34px",
                 });
-            } else {
-                $('section.navigation').removeClass('fixed');
-                $('header').css({
-                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "41px",
-                });
-                $('header .navicon').css({
-                    "top": "48px",
-                });
-            }
+            // } else {
+            //     $('section.navigation').removeClass('fixed');
+            //     $('header').css({
+            //         "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+            //         "padding": "50px 0"
+            //     });
+            //     $('header .member-actions').css({
+            //         "top": "41px",
+            //     });
+            //     $('header .navicon').css({
+            //         "top": "48px",
+            //     });
+            // }
         });
     });
     /***************** Smooth Scrolling ******************/
