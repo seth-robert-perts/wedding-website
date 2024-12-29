74811643cad2c0a39a89d7d615f6ef7c31ebd69b
Seth Perts <seth.robert.perts@gmail.com>
2024-12-28 21:11:24 -0500

    Tweak

--- js/scripts.js
+++ js/scripts.js
@@ -118,23 +118,23 @@ $(document).ready(function () {
     // });
     /***************** Smooth Scrolling ******************/
 
-    $(function () {
+    // $(function () {
 
-        $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+    //     $('a[href*=#]:not([href=#])').click(function () {
+    //         if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top - 90
-                    }, 2000);
-                    return false;
-                }
-            }
-        });
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             if (target.length) {
+    //                 $('html,body').animate({
+    //                     scrollTop: target.offset().top - 90
+    //                 }, 2000);
+    //                 return false;
+    //             }
+    //         }
+    //     });
 
-    });
+    // });
 
     /********************** Social Share buttons ***********************/
     var share_bar = document.getElementsByClassName('share-bar');
